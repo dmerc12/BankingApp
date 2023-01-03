1cff93bf7ab40fea2a8c5f8c195fb9c0d405204f
unknown <dylanmercer12@icloud.com>
2023-01-03 15:10:54 -0600

    customerPOM skeletons created

--- Tests/POMS/CustomerPOMs.py
+++ Tests/POMS/CustomerPOMs.py
@@ -6,3 +6,78 @@ class CustomerPOMs:
     def __init__(self, driver: WebDriver):
         self.driver = driver
 
+    def login_username_input(self):
+        pass
+
+    def login_password_input(self):
+        pass
+
+    def login_button(self):
+        pass
+
+    def continue_button(self):
+        pass
+
+    def create_first_name_input(self):
+        pass
+
+    def create_last_name_input(self):
+        pass
+
+    def create_username_input(self):
+        pass
+
+    def create_password_input(self):
+        pass
+
+    def create_email_input(self):
+        pass
+
+    def create_phone_number_input(self):
+        pass
+
+    def create_address_input(self):
+        pass
+
+    def create_new_customer_button(self):
+        pass
+
+    def update_first_name_input(self):
+        pass
+
+    def update_last_name_input(self):
+        pass
+
+    def update_username_input(self):
+        pass
+
+    def update_password_input(self):
+        pass
+
+    def update_email_input(self):
+        pass
+
+    def update_phone_number_input(self):
+        pass
+
+    def update_address_input(self):
+        pass
+
+    def update_customer_button(self):
+        pass
+
+    def manage_customer_information_collapse_button(self):
+        pass
+
+    def manage_customer_information_button(self):
+        pass
+
+    def update_customer_information_collapse_button(self):
+        pass
+
+    def delete_customer_collapse_button(self):
+        pass
+
+    def delete_customer_button(self):
+        pass
+    
