c24ff6ac41b6da9a44baf136486c73a0fb4e3657
unknown <dylanmercer12@icloud.com>
2023-01-03 14:47:56 -0600

    created POM file for customer tests for better organization, updated Environment.py to reflect such change

--- Tests/Environment.py
+++ Tests/Environment.py
@@ -1,10 +1,12 @@
 from behave.runner import Context
 from selenium.webdriver.chrome.webdriver import WebDriver
 from Tests.POMS.BankingPOMs import BankingPOMs
+from Tests.POMS.CustomerPOMs import CustomerPOMs
 
 def before_all(context: Context):
     context.driver = WebDriver()
-    context.secret_page_poms = BankingPOMs(context.driver)
+    context.banking_poms = BankingPOMs(context.driver)
+    context.customer_poms = CustomerPOMs(context.driver)
     context.driver.implicitly_wait(1)
 
 def after_all(context: Context):
---
+++ Tests/POMS/CustomerPOMs.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+
+class CustomerPOMs:
+
+    def __init__(self, driver: WebDriver):
+        self.driver = driver
+
+        
