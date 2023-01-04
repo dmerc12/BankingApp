4f37d781f4663983254d63bf809e49bbb1977a4d
unknown <dylanmercer12@icloud.com>
2023-01-03 18:09:21 -0600

    CustomerPOMs complete, need to use enter button click instead of a web element to grab when confronted with the alert

--- Tests/POMS/CustomerPOMs.py
+++ Tests/POMS/CustomerPOMs.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 
 
 class CustomerPOMs:
@@ -7,77 +9,101 @@ class CustomerPOMs:
         self.driver = driver
 
     def login_username_input(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "usernameInput")
+        return element
 
     def login_password_input(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "passwordInput")
+        return element
 
     def login_button(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "submitInfo")
+        return element
 
-    def continue_button(self):
-        pass
+    def indicate_new_customer(self):
+        element: WebElement = self.driver.find_element(By.ID, "indicateNewUser")
+        return element
 
     def create_first_name_input(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "firstNameInput")
+        return element
 
     def create_last_name_input(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "lastNameInput")
+        return element
 
     def create_username_input(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "usernameInput")
+        return element
 
     def create_password_input(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "passwordInput")
+        return element
 
     def create_email_input(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "emailInput")
+        return element
 
     def create_phone_number_input(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "phoneNumberInput")
+        return element
 
     def create_address_input(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "addressInput")
+        return element
 
     def create_new_customer_button(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "createCustomerButton")
+        return element
 
     def update_first_name_input(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "updatedFirstNameInput")
+        return element
 
     def update_last_name_input(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "updatedLastNameInput")
+        return element
 
     def update_username_input(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "updatedUsernameInput")
+        return element
 
     def update_password_input(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "updatedPasswordInput")
+        return element
 
     def update_email_input(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "updatedEmailAddressInput")
+        return element
 
     def update_phone_number_input(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "updatedPhoneNumberInput")
+        return element
 
     def update_address_input(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "updatedAddressInput")
+        return element
 
     def update_customer_button(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "updateCustomerButton")
+        return element
 
     def manage_customer_information_collapse_button(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "manageCustomerCollapseButton")
+        return element
 
     def manage_customer_information_button(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "navigateToManageCustomerButton")
+        return element
 
     def update_customer_information_collapse_button(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "updateCustomerCollapseButton")
+        return element
 
     def delete_customer_collapse_button(self):
-        pass
+        element: WebElement = self.driver.find_element(By.ID, "deleteCustomerCollapseButton")
+        return element
 
     def delete_customer_button(self):
-        pass
-    
+        element: WebElement = self.driver.find_element(By.ID, "deleteCustomerButton")
+        return element
