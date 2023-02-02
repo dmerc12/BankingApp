dcd4db819ad737121f327fd3e66b90bc926f98d5
unknown <dylanmercer12@icloud.com>
2023-02-01 18:13:54 -0600

    regular javascript and html home page complete and working

--- FrontEnd/Utility.js
+++ FrontEnd/Utility.js
@@ -14,37 +14,6 @@ for (i = 0; i < coll.length; i++) {
     });
 };
 
-function toggleVisibleLoginOrLogoutNav() {
-    
-}
-
-function toggleVisibleLoginOrLogoutNav() {
-    toggleVisibleNav = document.getElementById("navLoginRegisterLogout");
-    sessionId = window.sessionStorage.getItem("sessionId")
-
-    toggleVisibleNav.innerHTML = "";
-    if (sessionId == null) {
-        const loginButton = document.createElement("a");
-        loginButton.class = "nav-item nav-link";
-        loginButton.href = "Login.html";
-        loginButton.innerHTML = "Login";
-        toggleVisibleNav.appendChild(loginButton);
-
-        const registerButton = document.createElement("a");
-        registerButton.class = "nav-item nav-link";
-        registerButton.href = "NewCustomer.html";
-        registerButton.innerHTML = "Register";
-        toggleVisibleNav.appendChild(registerButton);
-    } else {
-        const logoutButton = document.createElement("a");
-        logoutButton.class = "nav-item nav-link";
-        logoutButton.href = "Login.html";
-        logoutButton.innerHTML = "Logout";
-        logoutButton.onclick = doLogout();
-        toggleVisibleNav.appendChild(logoutButton);
-    };
-};
-
 if (!window.sessionStorage.getItem("sessionId")) {
     alert("You do not have access to this page! Please continue to log in or create your own credentials!");
     window.location.href = "Login.html";
