9c5b657efda28c998683fdfb5eeeb4702498666a
unknown <dylanmercer12@icloud.com>
2023-02-06 18:07:26 -0600

    Updating customer page complete but untested

--- FrontEnd/Customer/Update/UpdateCustomer.js
+++ FrontEnd/Customer/Update/UpdateCustomer.js
@@ -13,6 +13,50 @@ if (!window.sessionStorage.getItem("sessionId")) {
     window.location.href = "../ManageLogin/Login.html";
 };
 
+async function doLogout() {
+    // initializing URL varible
+    logoutURL = "http://127.0.0.1:5000/logout"
+
+    // grabbing input from the DOM
+    const sessionId = window.sessionStorage.getItem("sessionId");
+
+    // preparing JSON
+    logoutDictionary = {
+        "sessionId": sessionId
+    };
+
+    // preparing request
+    let logoutRequest = {
+        method: "DELETE",
+        headers: {'Content-Type': "application/json"},
+        body: JSON.stringify(logoutDictionary)
+    };
+
+    // sending request and awaiting response
+    const response = await fetch(logoutURL, logoutRequest)
+
+    // handling API response approapriately
+    if (response.status === 201) {
+        const apiResponse = await response.json();
+        window.sessionStorage.removeItem("sessionId");
+        alert("Goodbye!");
+        window.location.href = "../ManageLogin/Login.html";
+    } else if (response.status === 400) {
+        const apiResponse = await response.json();
+        alert(`${apiResponse.message}`);
+        if (apiResponse.message === "Session has expired, please log in!") {
+            window.sessionStorage.removeItem("sessionId");
+            alert("Goodbye!");
+            window.location.href = "../ManageLogin/Login.html";
+        };
+    } else {
+        alert("Something went horribly wrong...");
+        window.sessionStorage.removeItem("sessionId");
+        alert("Goodbye!");
+        window.location.href = "../ManageLogin/Login.html";
+    };
+};
+
 async function loadCurrentInformation() {
     const loadCurrentInformationURL = "http://127.0.0.1:5000/load/customer/info";
 
