4aabf687e381a9e6f5b29e3b2b948c11409eb383
unknown <dylanmercer12@icloud.com>
2023-02-22 22:38:07 -0600

    flask front end rendering now correctly uses the form and displays error messages above with the bootstrap class, on success it redirects to the home page, the login route in the login blueprint needs to be deleted now as it will not be used

--- BankingApp/FlaskAPI/PageRenderingRoutes/CustomerRoutes.py
+++ BankingApp/FlaskAPI/PageRenderingRoutes/CustomerRoutes.py
@@ -1,9 +1,10 @@
 import datetime
 
-from flask import Blueprint, render_template, request, jsonify, current_app, make_response, redirect
+from flask import Blueprint, render_template, request, jsonify, current_app, make_response, redirect, url_for, flash
 
 from BankingApp.DAL.CustomerDAL.CustomerDALImplementation import CustomerDALImplementation
 from BankingApp.DAL.SessionDAL.SessionDALImplementation import SessionDALImplementation
+from BankingApp.Entities.FailedTransaction import FailedTransaction
 from BankingApp.Entities.Session import Session
 from BankingApp.SAL.CustomerSAL.CustomerSALImplementation import CustomerSALImplementation
 from BankingApp.SAL.SessionSAL.SessionSALImplementation import SessionSALImplementation
@@ -15,13 +16,34 @@ customer_sao = CustomerSALImplementation(customer_dao)
 session_dao = SessionDALImplementation()
 session_sao = SessionSALImplementation(session_dao)
 
-@customer_routes.route("/", methods=["GET"])
-@customer_routes.route("/login", methods=["GET"])
+@customer_routes.route("/", methods=["GET", "POST"])
+@customer_routes.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("Customer/Login.html")
+    if request.method == "POST":
+        try:
+            email = request.form["email"]
+            password = request.form["password"]
+            current_app.logger.info("Beginning API function login with data: " + str(email) + ", " + str(password))
+
+            result = customer_sao.service_login(email, password)
+            new_session_info = Session(0, result.customer_id, str(datetime.datetime.now()),
+                                       str(datetime.datetime.now() + datetime.timedelta(0, 0, 0, 0, 0, 1)))
+            new_session = session_sao.service_create_session(new_session_info)
+            current_app.logger.info("Finishing API function login with result: " + str(new_session))
+            login_response = make_response()
+            login_response.status_code = 201
+            login_response.set_cookie('session_id', str(new_session.session_id))
+            return redirect(url_for("customer_routes.home"))
+        except FailedTransaction as error:
+            current_app.logger.error("Error with API function login with error: " + str(error))
+            flash(str(error))
+            return render_template("Customer/Login.html"), 400
+    else:
+        return render_template("Customer/Login.html")
 
 @customer_routes.route("/home", methods=["GET"])
 def home():
+    session_id = request.cookies.get("session_id")
     return render_template("Main/Home.html")
 
 @customer_routes.route("/manage/customer", methods=["GET"])
@@ -30,22 +52,4 @@ def manage_customer():
 
 @customer_routes.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == "POST":
-        login_form = request.form
-        email = login_form["email"]
-        password = login_form["password"]
-        current_app.logger.info("Beginning API function login with data: " + str(email) + ", " + str(password))
-
-        result = customer_sao.service_login(email, password)
-        new_session_info = Session(0, result.customer_id, str(datetime.datetime.now()),
-                                   str(datetime.datetime.now() + datetime.timedelta(0, 0, 0, 0, 0, 1)))
-        new_session = session_sao.service_create_session(new_session_info)
-        result_dictionary = {
-            "sessionId": new_session.session_id
-        }
-        result_json = jsonify(result_dictionary)
-        current_app.logger.info("Finishing API function login with result: " + str(result_json))
-        login_response = make_response(result_json, 201)
-        return login_response, redirect(home.url)
-    else:
-        return render_template("Customer/Register.html")
+    return render_template("Customer/Register.html")
