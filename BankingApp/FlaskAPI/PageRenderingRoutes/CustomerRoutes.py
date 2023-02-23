ca24dc24e65843c746fbb786a95b212f5c56d533
unknown <dylanmercer12@icloud.com>
2023-02-22 23:22:38 -0600

    login page now works as intended, cookie wouldnt save to browser despite being set correctly so now flask session is being used to save the session id in the session storage, this has been manually tested

--- BankingApp/FlaskAPI/PageRenderingRoutes/CustomerRoutes.py
+++ BankingApp/FlaskAPI/PageRenderingRoutes/CustomerRoutes.py
@@ -1,6 +1,7 @@
 import datetime
 
-from flask import Blueprint, render_template, request, jsonify, current_app, make_response, redirect, url_for, flash
+from flask import Blueprint, render_template, request, jsonify, current_app, make_response, redirect, url_for, flash, \
+    session
 
 from BankingApp.DAL.CustomerDAL.CustomerDALImplementation import CustomerDALImplementation
 from BankingApp.DAL.SessionDAL.SessionDALImplementation import SessionDALImplementation
@@ -16,34 +17,8 @@ customer_sao = CustomerSALImplementation(customer_dao)
 session_dao = SessionDALImplementation()
 session_sao = SessionSALImplementation(session_dao)
 
-@customer_routes.route("/", methods=["GET", "POST"])
-@customer_routes.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        try:
-            email = request.form["email"]
-            password = request.form["password"]
-            current_app.logger.info("Beginning API function login with data: " + str(email) + ", " + str(password))
-
-            result = customer_sao.service_login(email, password)
-            new_session_info = Session(0, result.customer_id, str(datetime.datetime.now()),
-                                       str(datetime.datetime.now() + datetime.timedelta(0, 0, 0, 0, 0, 1)))
-            new_session = session_sao.service_create_session(new_session_info)
-            current_app.logger.info("Finishing API function login with result: " + str(new_session))
-            login_response = make_response()
-            login_response.status_code = 201
-            login_response.set_cookie('session_id', str(new_session.session_id))
-            return redirect(url_for("customer_routes.home"))
-        except FailedTransaction as error:
-            current_app.logger.error("Error with API function login with error: " + str(error))
-            flash(str(error))
-            return render_template("Customer/Login.html"), 400
-    else:
-        return render_template("Customer/Login.html")
-
 @customer_routes.route("/home", methods=["GET"])
 def home():
-    session_id = request.cookies.get("session_id")
     return render_template("Main/Home.html")
 
 @customer_routes.route("/manage/customer", methods=["GET"])
