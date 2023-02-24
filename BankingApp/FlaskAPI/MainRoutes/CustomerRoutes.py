d41050f7f6d611196901029c46f61ee0677e5185
unknown <dylanmercer12@icloud.com>
2023-02-23 19:44:22 -0600

    register page now works and has been manually tested

--- BankingApp/FlaskAPI/MainRoutes/CustomerRoutes.py
+++ BankingApp/FlaskAPI/MainRoutes/CustomerRoutes.py
@@ -5,6 +5,7 @@ from flask import Blueprint, render_template, request, jsonify, current_app, mak
 
 from BankingApp.DAL.CustomerDAL.CustomerDALImplementation import CustomerDALImplementation
 from BankingApp.DAL.SessionDAL.SessionDALImplementation import SessionDALImplementation
+from BankingApp.Entities.Customer import Customer
 from BankingApp.Entities.FailedTransaction import FailedTransaction
 from BankingApp.Entities.Session import Session
 from BankingApp.SAL.CustomerSAL.CustomerSALImplementation import CustomerSALImplementation
@@ -23,4 +24,27 @@ def manage_customer():
 
 @customer_routes.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # Handle form submission for new customer registration
+        try:
+            customer_info = request.form.to_dict()
+            current_app.logger.info("Beginning API function create customer with data: " + str(customer_info))
+            new_customer = Customer(0, customer_info["firstName"], customer_info["lastName"], customer_info["password"],
+                                    customer_info["email"], customer_info["phoneNumber"], customer_info["address"])
+            password_confirmation = customer_info["passwordConfirmation"]
+            result = customer_sao.service_create_customer(new_customer, password_confirmation)
+            result_dictionary = {
+                "firstName": result.first_name,
+                "lastName": result.last_name,
+                "email": result.email,
+                "phoneNumber": result.phone_number,
+                "address": result.address
+            }
+            current_app.logger.info("Finishing API function create customer with result: " + str(result_dictionary))
+            flash("Account created successfully. Please log in to continue.")
+            return redirect(url_for("login_route.login"))
+        except FailedTransaction as error:
+            current_app.logger.error("Error with API function create customer with error: " + str(error))
+            flash(str(error))
+    # Render registration form
     return render_template("Customer/Register.html")
