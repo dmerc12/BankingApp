26c6ad63aace85f97d6b66092000f8f984c919d3
unknown <dylanmercer12@icloud.com>
2023-02-28 23:43:26 -0600

    issue with viewing transactions was the typecasting being done in the route, now page works with amount but other fields are empty

--- BankingApp/FlaskAPI/TransactionRoutes/GetAllTransactionsBlueprint.py
+++ BankingApp/FlaskAPI/TransactionRoutes/GetAllTransactionsBlueprint.py
@@ -41,16 +41,16 @@ def get_all_transactions():
         current_app.logger.error("Error with API function get all transactions with message: " + str(error))
         return jsonify(message), 400
 
-@get_relevant_transactions.route("/account/<int:account_id>/transactions", methods=["GET"])
+@get_relevant_transactions.route("/account/<account_id>/transactions", methods=["GET"])
 def view_transactions(account_id):
     session_id = session.get('session_id')
-    current_app.logger.info("Beginning API function view transactions with data: " + str(session_id) + ", and " +
-                            str(account_id))
     session_verification = session_sao.service_get_session(session_id)
     if session_id is None:
         flash("Please log in!")
         return redirect(url_for("login_route.login"))
     else:
+        current_app.logger.info("Beginning API function view transactions with data: " + str(session_id) + ", and " +
+                                str(account_id))
         try:
             account = account_sao.service_get_account_by_id(account_id)
             if account.customer_id != session_verification.customer_id:
@@ -59,7 +59,7 @@ def view_transactions(account_id):
             else:
                 transactions = transaction_sao.service_get_all_transactions(account_id)
                 current_app.logger.info("Finishing API function view transactions with result: " + str(transactions))
-                return render_template("Transaction/Transactions.html", account=account, transactions=transactions)
+                return render_template("Transaction/Transactions.html", transaction_list=transactions)
         except FailedTransaction as error:
             current_app.logger.error("Error with API function view transactions with error: " + str(error))
             flash("Error viewing transactions: " + str(error))
