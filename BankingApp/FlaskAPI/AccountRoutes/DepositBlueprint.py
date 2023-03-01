e10aa48e4b4e276419feaa6c7780f53cca02c0e6
unknown <dylanmercer12@icloud.com>
2023-03-01 15:01:41 -0600

    deposit page complete and manually tested, altered create account amount input to have step similar to deposit amount so that decimal values are accepted

--- BankingApp/FlaskAPI/AccountRoutes/DepositBlueprint.py
+++ BankingApp/FlaskAPI/AccountRoutes/DepositBlueprint.py
@@ -49,10 +49,29 @@ def make_deposit():
         current_app.logger.error("Error with API function deposit with error: " + str(error))
         return jsonify(message), 400
 
-@do_deposit.route("/deposit", methods=["GET"])
+@do_deposit.route("/deposit", methods=["GET", "POST"])
 def deposit():
+    session_id = session["session_id"]
+    current_app.logger.info("Beginning API function deposit with data: " + str(session_id))
+    customer_id = session_sao.service_get_session(session_id).customer_id
+    accounts = account_sao.service_get_all_accounts(str(customer_id))
     if "session_id" not in session:
         flash("Please log in!")
         return redirect(url_for("login_route.login"))
     else:
-        return render_template("Account/Deposit.html")
+        if request.method == "POST":
+            try:
+                account_id = request.form["account_id"]
+                deposit_amount = request.form["amount"]
+                deposit_transaction = Transaction(0, str(datetime.datetime.now()), "Deposit", int(account_id),
+                                                  float(deposit_amount))
+                result = account_sao.service_deposit(account_id, deposit_amount)
+                transaction_sao.service_create_transaction(deposit_transaction)
+                current_app.logger.info("Finishing API function deposit with result: " + str(result))
+                flash("Deposit successful!")
+                return redirect(url_for("account_routes.manage_accounts"))
+            except FailedTransaction as error:
+                current_app.logger.error("Error with API function deposit with error: " + str(error))
+                flash(str(error))
+        else:
+            return render_template("Account/Deposit.html", account_list=accounts)
