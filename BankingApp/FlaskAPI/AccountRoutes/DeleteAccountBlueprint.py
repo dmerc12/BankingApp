ed2632019b8e7dab63d2de0f72d8fb997a74b96d
unknown <dylanmercer12@icloud.com>
2023-03-01 21:09:39 -0600

    delete account page complete and manually tested

--- BankingApp/FlaskAPI/AccountRoutes/DeleteAccountBlueprint.py
+++ BankingApp/FlaskAPI/AccountRoutes/DeleteAccountBlueprint.py
@@ -2,9 +2,11 @@ from flask import Blueprint, jsonify, request, current_app, session, url_for, re
 
 from BankingApp.DAL.BankAccountDAL.BankAccountDALImplementation import BankAccountDALImplementation
 from BankingApp.DAL.SessionDAL.SessionDALImplementation import SessionDALImplementation
+from BankingApp.DAL.TransactionDAL.TransactionDALImplementation import TransactionDALImplementation
 from BankingApp.Entities.FailedTransaction import FailedTransaction
 from BankingApp.SAL.BankAccountSAL.BankAccountSALImplementation import BankAccountSALImplementation
 from BankingApp.SAL.SessionSAL.SessionSALImplementation import SessionSALImplementation
+from BankingApp.SAL.TransactionSAL.TransactionSALImplementation import TransactionSALImplementation
 
 delete_this_account = Blueprint('delete_this_account', __name__)
 
@@ -12,6 +14,8 @@ session_dao = SessionDALImplementation()
 session_sao = SessionSALImplementation(session_dao)
 account_dao = BankAccountDALImplementation()
 account_sao = BankAccountSALImplementation(account_dao)
+transaction_dao = TransactionDALImplementation()
+transaction_sao = TransactionSALImplementation(transaction_dao)
 
 @delete_this_account.route("/delete/account/now", methods=["DELETE"])
 def delete_account_now():
@@ -32,10 +36,28 @@ def delete_account_now():
         current_app.logger.error("Error with API function delete account with error: " + str(error))
         return jsonify(message), 400
 
-@delete_this_account.route("/delete/account", methods=["GET"])
+@delete_this_account.route("/delete/account", methods=["GET", "POST"])
 def delete_account():
     if "session_id" not in session:
         flash(message="Please log in!", category="error")
         return redirect(url_for("login_route.login"))
     else:
-        return render_template("Account/DeleteAccount.html")
+        session_id = session["session_id"]
+        customer_id = session_sao.service_get_session(session_id).customer_id
+        accounts = account_sao.service_get_all_accounts(str(customer_id))
+        if request.method == "POST":
+            try:
+                account_id = request.form["account_id"]
+                current_app.logger.info("Beginning API function delete account with data: " + str(session_id) +
+                                        ", and " + str(account_id))
+                transaction_sao.service_delete_all_transactions(int(account_id))
+                result = account_sao.service_delete_account(account_id)
+                current_app.logger.info("Finishing API function delete account with result: " + str(result))
+                flash(message="Account successfully deleted!", category="success")
+                return redirect(url_for("account_routes.manage_accounts"))
+            except FailedTransaction as error:
+                current_app.logger.error("Error with API function delete account with error: " + str(error))
+                flash(message=str(error), category="error")
+                return render_template("Account/DeleteAccount.html", account_list=accounts)
+        else:
+            return render_template("Account/DeleteAccount.html", account_list=accounts)
