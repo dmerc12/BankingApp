edf04a635ed9251788eeae13dc93ffb5a2666889
unknown <dylanmercer12@icloud.com>
2023-03-04 19:38:34 -0600

    test and dal method for get all accounts for delete written, implemented, and passed need sal method for this before using it in delete all accounts or getting rid of that and implementing a catch to tell user to delete all accounts before deleting their information, but perhaps could warn user when deleting information that all accounts will be closed automtically, most likely the latter

--- BankingApp/Tests/UnitTests/DALTests/BankAccountDALTests.py
+++ BankingApp/Tests/UnitTests/DALTests/BankAccountDALTests.py
@@ -14,7 +14,11 @@ def test_get_account_by_id_success():
 
 def test_get_all_accounts_success():
     result = account_dao.get_all_accounts(test_account.customer_id)
-    assert len(result) >= 0
+    assert len(result) > 0
+
+def test_get_accounts_for_delete_success():
+    result = account_dao.get_accounts_for_delete(-2)
+    assert len(result) > 0
 
 def test_deposit_success():
     result = account_dao.deposit(test_account.account_id, 25.00)
