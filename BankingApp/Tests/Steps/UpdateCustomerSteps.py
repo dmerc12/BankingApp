c4e2373c3f412b1bdf11ea2655ed9198f2a12b50
unknown <dylanmercer12@icloud.com>
2023-07-17 22:03:35 -0500

    update information feature file complete with steps generated

---
+++ BankingApp/Tests/Steps/UpdateCustomerSteps.py
@@ -0,0 +1,62 @@
+from behave import when, then
+
+# when
+@when(u'I click the Manage Information navigation button')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I click the Manage Information navigation button')
+
+
+@when(u'I click the Update Your Information navigation button')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I click the Update Your Information navigation button')
+
+
+@when(u'I enter this is too long and so it will fail and bring a desired error in the update first name input')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I enter this is too long and so it will fail and bring a desired error in the update first name input')
+
+
+@when(u'I enter last in the update last name input')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I enter last in the update last name input')
+
+
+@when(u'I enter test@email.com in the update email address input')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I enter test@email.com in the update email address input')
+
+
+@when(u'I enter 1234567890 in the update phone number input')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I enter 1234567890 in the update phone number input')
+
+
+@when(u'I enter 123 T St in the update street address input')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I enter 123 T St in the update street address input')
+
+
+@when(u'I enter Oats in the update city input')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I enter Oats in the update city input')
+
+
+@when(u'I enter OH in the update state input')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I enter OH in the update state input')
+
+
+@when(u'I enter 43015 in the update zip code input')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I enter 43015 in the update zip code input')
+
+
+@when(u'I click the Update Information button')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I click the Update Information button')
+
+
+# then
+@then(u'I should be on a page with the title Updating Your Information')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should be on a page with the title Updating Your Information')
