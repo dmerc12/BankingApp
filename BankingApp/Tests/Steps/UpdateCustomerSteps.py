73bdc0028a9caf450371f92323e6fb52b35e44b7
unknown <dylanmercer12@icloud.com>
2023-07-19 23:05:32 -0500

    delete customer now works but update customer is not working, judging from the logs it's not even doing anything after attempting to login though the steps are saying that they're passing, this will take some troubleshooting

--- BankingApp/Tests/Steps/UpdateCustomerSteps.py
+++ BankingApp/Tests/Steps/UpdateCustomerSteps.py
@@ -1,67 +1,76 @@
+import time
+
 from behave import when, then
 
 # when
 @when(u'I click the Manage Information navigation button')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When I click the Manage Information navigation button')
+    context.customer_poms.manage_customer_navigation().click()
 
 
 @when(u'I click the Update Your Information navigation button')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When I click the Update Your Information navigation button')
+    context.customer_poms.update_customer_navigation().click()
 
 
 @when(u'I enter {first_name} in the update first name input')
 def step_impl(context, first_name: str):
-    raise NotImplementedError(u'STEP: When I enter this is too long and so it will fail and bring a desired error in the update first name input')
+    context.customer_poms.update_first_name_input().send_keys("")
+    context.customer_poms.update_first_name_input().send_keys(first_name)
 
 
 @when(u'I enter {last_name} in the update last name input')
 def step_impl(context, last_name: str):
-    raise NotImplementedError(u'STEP: When I enter last in the update last name input')
+    context.customer_poms.update_last_name_input().send_keys("")
+    context.customer_poms.update_last_name_input().send_keys(last_name)
 
 
 @when(u'I enter{email} in the update email address input')
 def step_impl(context, email: str):
-    raise NotImplementedError(u'STEP: When I enter updated@email.com in the update email address input')
+    context.customer_poms.update_email_input().send_keys("")
+    context.customer_poms.update_email_input().send_keys(email)
 
 
 @when(u'I enter {phone_number} in the update phone number input')
 def step_impl(context, phone_number: str):
-    raise NotImplementedError(u'STEP: When I enter 1234567890 in the update phone number input')
+    context.customer_poms.update_phone_number_input().send_keys("")
+    context.customer_poms.update_phone_number_input().send_keys(phone_number)
 
 
 @when(u'I enter {street_address} in the update street address input')
 def step_impl(context, street_address: str):
-    raise NotImplementedError(u'STEP: When I enter 123 T St in the update street address input')
+    context.customer_poms.update_street_address_input().send_keys("")
+    context.customer_poms.update_street_address_input().send_keys(street_address)
 
 
 @when(u'I enter {city} in the update city input')
 def step_impl(context, city: str):
-    raise NotImplementedError(u'STEP: When I enter Oats in the update city input')
+    context.customer_poms.update_city_input().send_keys("")
+    context.customer_poms.update_city_input().send_keys(city)
 
 
 @when(u'I enter {state} in the update state input')
 def step_impl(context, state: str):
-    raise NotImplementedError(u'STEP: When I enter OH in the update state input')
+    context.customer_poms.update_state_input().send_keys(state)
 
 
 @when(u'I enter {zip_code} in the update zip code input')
 def step_impl(context, zip_code: str):
-    raise NotImplementedError(u'STEP: When I enter 43015 in the update zip code input')
+    context.customer_poms.update_zip_code_input().send_keys(zip_code)
 
 
 @when(u'I click the Update Information button')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When I click the Update Information button')
+    context.customer_poms.update_button().click()
 
 
 # then
 @then(u'I should be on a page with the title Updating Your Information')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I should be on a page with the title Updating Your Information')
+    assert context.driver.title == "Updating Your Information"
 
 
 @then(u'I should be on a page with the title Managing Your Current Information')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I should be on a page with the title Managing Your Current Information')
+    time.sleep(5)
+    assert context.driver.title == "Managing Your Current Information"
