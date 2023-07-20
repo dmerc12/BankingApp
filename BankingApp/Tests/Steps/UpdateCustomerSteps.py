bbb4fce42db6836a72663f1b83fc6e9af73e7f42
unknown <dylanmercer12@icloud.com>
2023-07-19 23:17:30 -0500

    update customer feature and steps now complete with all steps passing, had to use a clear function on the input fields prior to putting in the new info or it would just add the new right after the old, working as intended now

--- BankingApp/Tests/Steps/UpdateCustomerSteps.py
+++ BankingApp/Tests/Steps/UpdateCustomerSteps.py
@@ -1,5 +1,3 @@
-import time
-
 from behave import when, then
 
 # when
@@ -15,37 +13,37 @@ def step_impl(context):
 
 @when(u'I enter {first_name} in the update first name input')
 def step_impl(context, first_name: str):
-    context.customer_poms.update_first_name_input().send_keys("")
+    context.customer_poms.update_first_name_input().clear()
     context.customer_poms.update_first_name_input().send_keys(first_name)
 
 
 @when(u'I enter {last_name} in the update last name input')
 def step_impl(context, last_name: str):
-    context.customer_poms.update_last_name_input().send_keys("")
+    context.customer_poms.update_last_name_input().clear()
     context.customer_poms.update_last_name_input().send_keys(last_name)
 
 
 @when(u'I enter{email} in the update email address input')
 def step_impl(context, email: str):
-    context.customer_poms.update_email_input().send_keys("")
+    context.customer_poms.update_email_input().clear()
     context.customer_poms.update_email_input().send_keys(email)
 
 
 @when(u'I enter {phone_number} in the update phone number input')
 def step_impl(context, phone_number: str):
-    context.customer_poms.update_phone_number_input().send_keys("")
+    context.customer_poms.update_phone_number_input().clear()
     context.customer_poms.update_phone_number_input().send_keys(phone_number)
 
 
 @when(u'I enter {street_address} in the update street address input')
 def step_impl(context, street_address: str):
-    context.customer_poms.update_street_address_input().send_keys("")
+    context.customer_poms.update_street_address_input().clear()
     context.customer_poms.update_street_address_input().send_keys(street_address)
 
 
 @when(u'I enter {city} in the update city input')
 def step_impl(context, city: str):
-    context.customer_poms.update_city_input().send_keys("")
+    context.customer_poms.update_city_input().clear()
     context.customer_poms.update_city_input().send_keys(city)
 
 
@@ -72,5 +70,4 @@ def step_impl(context):
 
 @then(u'I should be on a page with the title Managing Your Current Information')
 def step_impl(context):
-    time.sleep(5)
     assert context.driver.title == "Managing Your Current Information"
