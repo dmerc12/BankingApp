9e20ffb6627805331ac8148ad14fde331c77fd45
unknown <dylanmercer12@icloud.com>
2023-09-01 19:03:58 -0500

    now that refactor is complete the old files are deleted and project minimized to working portion

--- run.py
+++
@@ -1,7 +0,0 @@
-from BankingApp.DAL.Database import config
-from BankingApp.FlaskAPI import create_back_end_api
-
-app = create_back_end_api(config)
-
-if __name__ == "__main__":
-    app.run(debug=True)
