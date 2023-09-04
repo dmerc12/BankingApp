17351fa6480d039bf83ebc30091cd35efc55bef5
unknown <dylanmercer12@icloud.com>
2023-09-03 22:59:58 -0500

    routes set up and linked to pages that are empty, navbar is also empty, project ready to be worked

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ToastContainer } from 'react-toastify';
+import './App.css';
+import { Navbar } from './components/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ViewTransactions } from './pages/ViewTransactions';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
+import { ManageInformation } from './pages/ManageInformation';
+import { ManageAccounts } from './pages/ManageAccounts';
+import { Home } from './pages/Home';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route index path='/' element={<Login />}/>
+          <Route index path='/login' element={<Login />}/>
+          <Route index path='/register' element={<Register />}/>
+          <Route index path='/home' element={<Home />}/>
+          <Route index path='/manage/information' element={<ManageInformation />}/>
+          <Route index path='/manage/accounts' element={<ManageAccounts />}/>
+          <Route index path='/manage/accounts' element={<ViewTransactions />}/>
+        </Routes>
+      </BrowserRouter>
+
+      <ToastContainer position='top-center' newestOnTop autoClose={3000} hideProgressBar theme='colored' limit={1} closeOnClick />
     </>
   )
 }
