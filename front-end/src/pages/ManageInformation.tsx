abb80d9d629d23ac2acac165f161f5f93376e477
unknown <dylanmercer12@icloud.com>
2023-09-06 23:44:22 -0500

    manage information page complete and limited access to those with the session ID, linked relevant components

--- front-end/src/pages/ManageInformation.tsx
+++ front-end/src/pages/ManageInformation.tsx
@@ -1,7 +1,32 @@
+import { UpdateForm } from "../components/ui/customer/UpdateForm";
+import { ChangePasswordForm } from "../components/ui/customer/ChangePasswordForm";
+import { DeleteForm } from "../components/ui/customer/DeleteForm";
+import { useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import { toast } from 'react-toastify';
+import Cookies from "js-cookie";
+
 export const ManageInformation = () => {
+    const navigate = useNavigate();
+    const sessionId = Cookies.get('sessionId');
+
+    useEffect(() => {
+        if (!sessionId) {
+            navigate('/login');
+            toast.info("Please login or register to gain access!", {
+                toastId: 'custom'
+            })
+        }
+    }, [navigate, sessionId])
+
     return (
         <>
-        
+            <h1>Manage Information Below!</h1>
+            <div className="action-btn-container">
+                <UpdateForm />
+                <ChangePasswordForm />
+                <DeleteForm />
+            </div>
         </>
     )
 }
