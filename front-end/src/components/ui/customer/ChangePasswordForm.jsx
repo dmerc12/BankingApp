7da0744304c46790663fa800e5850043ccd28494
unknown <dylanmercer12@icloud.com>
2023-09-12 09:39:09 -0500

    change password form set up with only rendering functionality left before testing

--- front-end/src/components/ui/customer/ChangePasswordForm.jsx
+++ front-end/src/components/ui/customer/ChangePasswordForm.jsx
@@ -1,4 +1,94 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useFetch } from "../../../hooks/useFetch";
+import { toast } from "react-toastify";
+import { Modal } from "../../Modal";
+import { FaSpinner, FaSync } from "react-icons/fa";
+import { AiOutlineExclamationCircle } from "react-icons/ai";
+import Cookies from "js-cookie";
+
 export const ChangePasswordForm = () => {
+    const [changePasswordForm, setChangePasswordForm] = useState({
+        sessionId: 0,
+        password: '',
+        confirmationPassword: ''
+    });
+    const [visible, setVisible] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [failedToFetch, setFailedToFetch] = useState(false);
+
+    const sessionId = Cookies.get('sessionId');
+    
+    const { fetchData } = useFetch();
+
+    const navigate = useNavigate();
+
+    const showModal = () => {
+        setVisible(true);
+    };
+    
+    const closeModal = () => {
+        setVisible(false);
+    };
+
+    const goBack = () => {
+        setFailedToFetch(false);
+    };
+
+    const onChange = (event) => {
+        const { name, value } = event.target;
+        setChangePasswordForm((prevForm) => ({
+            ...prevForm,
+            [name]: value
+        }));
+    };
+
+    useEffect(() => {
+        setChangePasswordForm((prevForm) => ({
+            ...prevForm,
+            sessionId: sessionId
+        }))
+    }, [sessionId])
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setLoading(true);
+        setFailedToFetch(false);
+        try {
+            const { responseStatus, data } = await fetchData('/change/password/now', 'PUT', changePasswordForm);
+
+            if (responseStatus === 200) {
+                window.location.reload();
+                setVisible(false);
+                setLoading(false);
+                toast.success("Password successfully changed!", {
+                    toastId: 'customId'
+                });
+            } else if (responseStatus === 400) {
+                throw new Error(`${data.message}`);
+            } else {
+                throw new Error("Something went horribly wrong!");
+            }
+        } catch (error) {
+            if (error.message === "No session found, please try again!" || error.message === "Session has expired, please log in!") {
+                Cookies.remove('sessionId');
+                navigate('/login');
+                setLoading(false);
+                toast.warn(error.message, {
+                    toastId: "customId"
+                });
+            } else if (error.message === "Failed to fetch") {
+                setFailedToFetch(true);
+                setLoading(false);
+            } else {
+                setLoading(false);
+                toast.warn(error.message, {
+                    toastId: "customId"
+                });
+            }
+        }
+    }
+
     return (
         <>
         
