db1170e524153d8351294b553d892b32fdebf5ca
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-10-19 22:44:58 -0500

    adds complete steps for create account feature file

---
+++ Back-End/Tests/steps/AccountSteps.py
@@ -0,0 +1,20 @@
+from behave import when
+
+@when(u'I click the manage accounts button')
+def step_impl(context):
+    context.account_poms.manage_accounts_button().click()
+
+
+@when(u'I click the create account modal')
+def step_impl(context):
+    context.account_poms.create_account_modal().click()
+
+
+@when(u'I input the {amount} in the starting balance input')
+def step_impl(context, amount):
+    context.account_poms.create_account_starting_balance_input().send_keys(amount)
+
+
+@when(u'I click the create account button')
+def step_impl(context):
+    context.account_poms.create_account_button().click()
