b6801840ce06e93b73a71b72ad08cae01c1b211f
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-10-22 21:01:51 -0500

    updates install.sh to almost be complete, lacking steps for uninstall if failure is encountered though it is checking for and tracking errors or failures in all steps and tests

--- install.sh
+++ install.sh
@@ -1,66 +1,101 @@
 #!/bin/bash
 
+# Initialize a variable to count failures
+failures=0
+# Function to run a command and check for errors
+run_command() {
+  "$@"
+  local status=$?
+  if [ $status -ne 0 ]; then
+    echo "Error: $1 command failed with status $status."
+    failures=failures+1
+    exit "total failures: $failures"
+  fi
+}
+
 # Prompt for PostgreSQL environment variables
 read -p "Enter your PostgreSQL username: " postgres_user
 read -s -p "Enter your PostgreSQL password: " postgres_password
 
 # Update and upgrade the system
-sudo apt update
-sudo apt upgrade -y
+run_command sudo apt update
+run_command sudo apt upgrade -y
 
 # Install required software
-sudo apt install -y docker.io docker-compose
-sudo apt install -y python3
-sudo apt install -y python3-pip
-sudo apt install -y nodejs
-sudo apt install -y npm
+run_command sudo apt install -y docker.io docker-compose
+run_command sudo apt install -y python3
+run_command sudo apt install -y python3-pip
+run_command sudo apt install -y nodejs
+run_command sudo apt install -y npm
 
 # Start Docker and enable it to start on boot
-sudo systemctl start docker
-sudo systemctl enable docker
+run_command sudo systemctl start docker
+run_command sudo systemctl enable docker
 
 # Install PostgreSQL Docker container with user-provided credentials
-docker run --name postgres -e POSTGRES_USER="$postgres_user" -e POSTGRES_PASSWORD="$postgres_password" -d -p 5432:5432 postgres:latest
+run_command docker run --name postgres -e POSTGRES_USER="$postgres_user" -e POSTGRES_PASSWORD="$postgres_password" -d -p 5432:5432 postgres:latest
 
 # Install back-end dependencies
 cd BankingApp/Back-End
-pip3 install -r requirements.txt
+run_command pip3 install -r requirements.txt
 
 # Configure database config file and setup database
 cd Database
 sed -i "s/user=os.environ.get(\"USER\"),/user=\"$postgres_user\",/" config.py
 sed -i "s/password=os.environ.get(\"PASSWORD\"),/password=\"$postgres_password\",/" config.py
-python3 DatabaseSetup.py
+run_command python3 DatabaseSetup.py
 
 
-# Run unit tests, then start back-end server for end-to-end tests
-cd Tests/UnitTests/DALTests/
-python3 -m pytest SessionDALTests.py
-python3 -m pytest CustomerDALTests.py
-python3 -m pytest BankAccountDALTests.py
+# Run unit tests
+cd Tests/UnitTests/DALTests
+run_command python3 -m pytest SessionDALTests.py
+run_command python3 -m pytest CustomerDALTests.py
+run_command python3 -m pytest BankAccountDALTests.py
 cd ../../../Database
-python3 ResetDatabaseTest.py
+run_command python3 ResetDatabaseTest.py
 cd ../Tests/UnitTests/SALTests
-python3 -m pytest SessionSALTests.py
-python3 -m pytest CustomerSALTests.py
-python3 -m pytest BankAccountSALTests.py
+run_command python3 -m pytest SessionSALTests.py
+run_command python3 -m pytest CustomerSALTests.py
+run_command python3 -m pytest BankAccountSALTests.py
 cd ../../../Database
-python3 ResetDatabaseTest.py
+run_command python3 ResetDatabaseTest.py
+
+# Start the back-end server for end-to-end tests
 cd ..
 nohup python3 run.py &
 
 # Install Node.js dependencies and start the react front-end for end-to-end tests
 cd ../../front-end
-npm install
+run_command npm install
 pm2 start npm --name "front-end" --start
 
 # Run end-to-end tests
 cd ../Back-End/Tests
-behave Features
+run_command behave Features
 
 # Reset database for production
-cd ..//Database
-python3 ResetDatabaseProduction.py
+cd ../Database
+run_command python3 ResetDatabaseProduction.py
 
 # Tell user if install is successful or unsuccessful (if statements will probably need to be included)
+if [ $failures -gt 0 ]; then
+  echo "Installation encountered $failures failures."
+  read -p "Do you want to try again (t) or uninstall (u)? " choice
+  if [ "$choice" == "t" ]; then
+    # Add uninstall steps here
+    echo "Uninstalling..."
+    # Run your uninstall script
+    # ...
 
+    # Restart the installation script
+    exec "$0"
+  elif [ "$choice" == "u" ]; then
+    # Run your uninstall script
+    echo "Uninstalling..."
+    # ...
+  else
+    echo "Invalid choice. Exiting."
+  fi
+else
+  echo "Installation was successful."
+fi
