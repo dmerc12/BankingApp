70b632da81df58c6b1c561b87776d0a9ccf7902a
unknown <dylanmercer12@icloud.com>
2023-10-26 19:38:37 -0500

    replaces previous custom toast component in progress with another one, page renders and works but the toast notification is not appearing, though it is not styed yet with Toast.css so that may change things, integrated into login form and app.jsx files though

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import ToastProvider from './components/ui/toast/ToastProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ToastProvider>
-      <App />
-    </ToastProvider>
+    <App />
   </React.StrictMode>,
 )
