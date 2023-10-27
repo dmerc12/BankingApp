2a9bf357e7cb3a8ba570e7dcb8af570ce5a09fdc
unknown <dylanmercer12@icloud.com>
2023-10-26 19:34:20 -0500

    adds new useToast hook

--- front-end/src/hooks/useToast.js
+++ front-end/src/hooks/useToast.js
@@ -1,4 +1,22 @@
-import { useContext } from "react";
-import { ToastContext } from "../components/ui/toast/toast-context";
+import { useState, useCallback } from 'react';
 
-export const useToast = () => useContext(ToastContext);
+const DEFAULT_OPTIONS = {
+    autoClose: 3000,
+    position: 'top-center'
+};
+
+export const useToast = (defaultOptions = {}) => {
+    const [toasts, setToasts] = useState([]);
+
+    const addToast = useCallback((message, options = {}) => {
+        const id = new Date().getTime();
+        const mergedOptions = { ...DEFAULT_OPTIONS, ...defaultOptions, ...options };
+        setToasts((prevToasts) => [...prevToasts, { id, message, options: mergedOptions}]);
+    }, [defaultOptions]);
+
+    const removeToast = useCallback((id) => {
+        setToasts((prevToasts) => prevToasts.filter((toast) => toast.id !== id));
+    }, []);
+
+    return { toasts, addToast, removeToast };
+};
