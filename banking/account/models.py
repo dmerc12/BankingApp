80751776dfdd404e681994cf82b2393a8a227468
unknown <dylanmercer12@icloud.com>
2024-02-10 18:12:56 -0600

    adds functionality for creating account and generates an initial transaction with the account

--- banking/account/models.py
+++ banking/account/models.py
@@ -7,6 +7,11 @@ class Account(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     balance = models.DecimalField(max_digits=11, decimal_places=2)
     account_number = models.CharField(max_length=10, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.account_number
 
     # Save generated account number to account
     def save(self, *args, **kwargs):
@@ -32,14 +37,19 @@ TRANSACTION_TYPES = [
 
 # Transactions for deposits, expenses, and transfers
 class Transaction(models.Model):
-    # account may need to be a list of accounts involved, maybe through many to many field TransactionAccount
     accounts = models.ManyToManyField(Account, through='TransactionAccount')
     amount = models.DecimalField(max_digits=11, decimal_places=2)
-    type = models.CharField(max_length=8)
-    notes = models.TextField(max_length=250)
+    type = models.CharField(max_length=8, choices=TRANSACTION_TYPES)
+    notes = models.TextField(max_length=250, blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
 
 # Accounts involved in transactions incase more than one
 class TransactionAccount(models.Model):
     transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Transaction: {self.transaction}, Account: {self.account.account_number}'
--- banking/account/views.py
+++ banking/account/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.db import transaction
+from .models import *
+from .forms import *
 
-# Create your views here.
+# Create account view
+def create_account(request):
+    if request.method == 'POST':
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            opening_balance = form.cleaned_data['opening_balance']
+            opening_notes = form.cleaned_data['opening_notes']
+
+            # Ensuring balance is positive and non-zero
+            if opening_balance <= 0:
+                messages.error(request, 'Opening balance must be positive and non-zero, please try again!')
+                return redirect('create-account')
+            with transaction.atomic():
+                account = Account(user=request.user, balance=opening_balance)
+                account.save()
+                initial_transaction = Transaction(amount=opening_balance, notes=opening_notes, type=DEPOSIT)
+                initial_transaction.save()
+                transaction_account = TransactionAccount(account=account, transaction=initial_transaction)
+                transaction_account.save()
+            messages.success(request, 'Account successfully created!')
+            return redirect('home')
+    else:
+        form = AccountForm()
+    return render(request, 'account/create.html', {'form': form})
