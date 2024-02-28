23d5fbab9ece1a278ba32b71177b91b17a1afd9a
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2024-02-27 22:34:19 -0600

    adds steps for register feature, adds pom files for navbar and side navbar

--- tests/environment.py
+++ tests/environment.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.safari.webdriver import WebDriver
-from selenium.webdriver.edge.webdriver import WebDriver
+# from selenium.webdriver.edge.webdriver import WebDriver
 from poms.users.register import RegisterPage
 from poms.users.login import LoginPage
 from behave.runner import Context
@@ -7,10 +7,11 @@ from behave.runner import Context
 # Setup for webdriver and POM files before selenium tests
 def before_all(context: Context):
     context.driver = WebDriver()
+    
     # register POM files below
     context.login_poms = LoginPage(context.driver)
     context.register_poms = RegisterPage(context.driver)
-    
+
     context.driver.implicitly_wait(1)
 
 # Cleanup for webdriver after selenium tests
--- tests/steps/users.py
+++ tests/steps/users.py
@@ -1,17 +1,73 @@
-from behave import when
+from behave import given, when
 
+## Login steps
 # When step for entering username on login page
 @when(u'I enter {username} in the login username field')
 def step_impl(context, username):
-    context.login_poms.enter_login_username(username)
+    context.login_poms.enter_username_input(username)
     
 # When step for entering password on login page
 @when(u'I enter {password} in the login password field')
 def step_impl(context, password):
-    context.login_poms.enter_login_password(password)
+    context.login_poms.enter_password_input(password)
     
 # When step for clicking the login button
 @when(u'I click the login button')
 def step_impl(context):
     context.universal_poms.click_login_button()
+    
+
+## Register steps
+# Given step for going to the register page
+@given(u'I am on the register page')
+def step_impl(context):
+    context.driver.get('http://127.0.0.1:8000/users/register/')
+
+
+# When step for entering username on register page
+@when(u'I enter {username} in the register username field')
+def step_impl(context, username):
+    context.register_poms.enter_username_input(username)
+
+
+# When step for entering first name on register page
+@when(u'I enter {first_name} in the register first name field')
+def step_impl(context, first_name):
+    context.register_poms.enter_first_name_input(first_name)
+
+
+# When step for entering last name on register page
+@when(u'I enter {last_name} in the register last name field')
+def step_impl(context, last_name):
+    context.register_poms.enter_last_name_input(last_name)
+
+    
+# When step for entering email on register page
+@when(u'I enter {email} in the register email field')
+def step_impl(context, email):
+    context.register_poms.enter_email_input(email)
+
+    
+# When step for entering phone number on register page
+@when(u'I enter {phone_number} in the register phone number field')
+def step_impl(context, phone_number):
+    context.register_poms.enter_phone_number_input(phone_number)
+    
+    
+# When step for entering password1 on register page
+@when(u'I enter {password1} in the register password1 field')
+def step_impl(context, password1):
+    context.register_poms.enter_password1_input(password1)
+    
+    
+# When step for entering password2 on register page
+@when(u'I enter {password2} in the register password2 field')
+def step_impl(context, password2):
+    context.register_poms.enter_password2_input(password2)
+
+    
+# When step for clicking the register button
+@when(u'I click the register button')
+def step_impl(context):
+    context.register_poms.click_register_button()
     
